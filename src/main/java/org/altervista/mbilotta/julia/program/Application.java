f5fca5dd1bfbb691012032e794a1d43b704df89a
mbilotta <maurizeio@yahoo.it>
2020-01-25 20:30:15 +0100

    version bump

--- src/main/java/org/altervista/mbilotta/julia/program/Application.java
+++ src/main/java/org/altervista/mbilotta/julia/program/Application.java
@@ -132,7 +132,7 @@ import org.altervista.mbilotta.julia.program.parsers.RepresentationPlugin;
 
 public class Application {
 	
-	public static final String VERSION = "1.0.11";
+	public static final String VERSION = "1.1.0";
 
 	public static final class Image {
 		private PluginInstance<NumberFactoryPlugin> numberFactoryInstance;
