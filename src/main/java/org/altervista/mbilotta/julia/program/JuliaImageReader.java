05b2363dbbe25954616e252a21c364f19555a359
mbilotta <maurizeio@yahoo.it>
2020-12-07 13:09:38 +0100

    support for re-run JIM file reading

--- src/main/java/org/altervista/mbilotta/julia/program/JuliaImageReader.java
+++ src/main/java/org/altervista/mbilotta/julia/program/JuliaImageReader.java
@@ -187,58 +187,68 @@ public class JuliaImageReader extends BlockingSwingWorker<Void> {
 	public void read() {
 		try (ZipFile zipFile = new ZipFile(file)) {
 
-			publishToGui("number factory...");
-			numberFactoryInstance = readNumberFactory(zipFile);
-			if (isCancelled()) return;
+			if (numberFactoryInstance == null) {
+				publishToGui("number factory...");
+				numberFactoryInstance = readNumberFactory(zipFile);
+				if (isCancelled()) return;	
+			}
 			setGuiProgress(12);
 
-			publishToGui("formula...");
-			formulaInstance = readFormula(zipFile);
-			if (isCancelled()) return;
+			if (formulaInstance == null) {
+				publishToGui("formula...");
+				formulaInstance = readFormula(zipFile);
+				if (isCancelled()) return;	
+			}
 			setGuiProgress(24);
 
-			publishToGui("representation...");
-			representationInstance = readRepresentation(zipFile);
-			if (isCancelled()) return;
+			if (representationInstance == null) {
+				publishToGui("representation...");
+				representationInstance = readRepresentation(zipFile);
+				if (isCancelled()) return;
+			}
 			setGuiProgress(36);
 
-			publishToGui("rectangle...");
-			ZipEntry entry = zipFile.getEntry("rectangle");
-			if (entry == null) {
-				addFatalError(null);
-				errorOutput.println("could not find rectangle zip entry.");
-			} else {
-				try (InputStream is = zipFile.getInputStream(entry);
-						ObjectInputStream ois = new ObjectInputStream(is)) {
-					rectangle = readNonNull(ois, "rectangle", Rectangle.class);
-					forceEqualScalesOut.set(ois.readBoolean());
-				} catch (ClassNotFoundException | IOException e) {
-					addFatalError(entry);
-					errorOutput.printStackTrace(e);
+			if (rectangle == null) {
+				publishToGui("rectangle...");
+				ZipEntry entry = zipFile.getEntry("rectangle");
+				if (entry == null) {
+					addFatalError(null);
+					errorOutput.println("could not find rectangle zip entry.");
+				} else {
+					try (InputStream is = zipFile.getInputStream(entry);
+							ObjectInputStream ois = new ObjectInputStream(is)) {
+						rectangle = readNonNull(ois, "rectangle", Rectangle.class);
+						forceEqualScalesOut.set(ois.readBoolean());
+					} catch (ClassNotFoundException | IOException e) {
+						addFatalError(entry);
+						errorOutput.printStackTrace(e);
+					}
+					if (isCancelled()) return;
 				}
-				if (isCancelled()) return;
 			}
 			setGuiProgress(48);
 
-			entry = zipFile.getEntry("juliaSetPoint");
-			if (entry == null) {
-				juliaSetPointOut.set(null);
-			} else {
-				publishToGui("julia set point...");
-				try (InputStream is = zipFile.getInputStream(entry);
-						ObjectInputStream ois = new ObjectInputStream(is)) {
-					JuliaSetPoint juliaSetPoint = readNonNull(ois, "juliaSetPoint", JuliaSetPoint.class);
-					juliaSetPointOut.set(juliaSetPoint);
-				} catch (ClassNotFoundException | IOException e) {
-					addFatalError(entry);
-					errorOutput.printStackTrace(e);
+			if (!juliaSetPointOut.isSet()) {
+				ZipEntry entry = zipFile.getEntry("juliaSetPoint");
+				if (entry == null) {
+					juliaSetPointOut.set(null);
+				} else {
+					publishToGui("julia set point...");
+					try (InputStream is = zipFile.getInputStream(entry);
+							ObjectInputStream ois = new ObjectInputStream(is)) {
+						JuliaSetPoint juliaSetPoint = readNonNull(ois, "juliaSetPoint", JuliaSetPoint.class);
+						juliaSetPointOut.set(juliaSetPoint);
+					} catch (ClassNotFoundException | IOException e) {
+						addFatalError(entry);
+						errorOutput.printStackTrace(e);
+					}
+					if (isCancelled()) return;
 				}
-				if (isCancelled()) return;
 			}
 			setGuiProgress(60);
 
 			if (fatalCount == 0 && loadIntermediateImage) {
-				entry = zipFile.getEntry("intermediateImage");
+				ZipEntry entry = zipFile.getEntry("intermediateImage");
 				if (entry != null) {
 					hasIntermediateImage = true;
 					publishToGui("intermediate image...");
